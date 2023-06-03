1faf3a7d2a0e10cd8f06c23f860d8f59d3db155c
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2023-06-03 06:58:42 +0300

    add before_save method to save emails in proper format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 50 }
   validates :email, presence: true, uniqueness: true, length: {maximum: 105}, format: { with: URI::MailTo::EMAIL_REGEXP }
 
