10c1f0c9c8e351592ed27746dfe1fe61762175e2
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2023-06-03 20:51:42 +0300

    add error messages for forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 50 }
-  validates :email, presence: true, uniqueness: true, length: {maximum: 105}, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, uniqueness: true, length: {maximum: 105}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 
   has_many :articles
   has_secure_password
+  before_save { self.email = email.downcase }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "/about", to: 'pages#about'
+  get "/register", to: 'users#new'
+  post '/users', to: 'users#create'
   resources :articles
   root "articles#index"
 end
